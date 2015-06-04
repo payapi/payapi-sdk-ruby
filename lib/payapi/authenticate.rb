a212b971ff41265b423565cb8ecc139ece475046
Marko <marko@messagingapi.io>
2015-06-04 18:27:44 +0000

    Password is no longer sent in plain text.

--- lib/payapi/authenticate.rb
+++ lib/payapi/authenticate.rb
@@ -1,32 +1,35 @@
 require 'rest-client'
+require 'jwt'
 module PayApi
 
-  RestClient.add_before_execution_proc do |req, params|
-    #content_type: :json,
-    #  accept: :json
-  end
-
   class Authenticate
-    attr_reader :attributes, :site, :api_key, :password, :private_key
+    attr_reader :attributes, :private_key, :site
 
+    # attributes = {
+    #   api_key: <your api key>,
+    #   site: <site of authentication>,
+    #   private_key: <your private key>,
+    #   password: <your api key's password>
+    # }
     def initialize(attributes)
       @attributes = attributes
+      @site = attributes.fetch(:site)
+      @private_key = attributes.fetch(:private_key)
+      RestClient.add_before_execution_proc do |req, params|
+        req['alg'] = 'HS512'
+      end
     end
 
     def call
-      @site = RestClient::Resource.new(attributes.fetch(:site))
-      @api_key = attributes.fetch(:api_key)
-      @password = attributes.fetch(:password)
-      @private_key = attributes.fetch(:private_key)
-      begin
-        data = {
+      resource = RestClient::Resource.new(@site, { headers: {content_type: :json, accept: :json }})
+      api_key = attributes.fetch(:api_key)
+      data = {user: {'key': api_key, 'password': attributes.fetch(:password)}}
+      token = JWT.encode data, @private_key, 'HS512'
+        params = {
           api_key: api_key,
-          password: password
+          token: token
         }.to_json
-        response = site['/auth/login'].post data
-      rescue => e
-        e.response.code
-      end
+        response = resource['/auth/login'].post params
     end
   end
 end
