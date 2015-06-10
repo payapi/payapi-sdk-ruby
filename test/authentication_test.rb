091447cd9f89714aa5ee1b8a226ced062731aa1e
Marko <marko@messagingapi.io>
2015-06-10 15:38:36 +0000

    Split authentication test into separate file

---
+++ test/authentication_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+require 'payapi/authenticate'
+require 'jwt'
+
+class TestPayApiAuthentication < Minitest::Test
+  def test_authentication_headers
+    site = 'input.payapi.io'
+    endpoint = "#{site}/auth/login"
+    stub_request(:post, endpoint)
+    params = {
+      site: site,
+      key: '123',
+      password: 'password',
+      private_key: 'private_key'
+    }
+    PayApi::Authenticate.new(params).call
+    assert_requested :post, endpoint,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'alg': 'HS512'
+      },
+      times: 1
+  end
+
+  def test_password_is_encrypted_using_private_key
+    site = 'input.payapi.io'
+    private_key = 'private_key'
+    password = 'password'
+    api_key = '123'
+    endpoint = "#{site}/auth/login"
+    stub_request(:post, endpoint)
+
+    params = {
+      site: site,
+      key: api_key,
+      password: password,
+      private_key: private_key
+    }
+
+    PayApi::Authenticate.new(params).call
+    data = {apiKey: {key: api_key, 'password': password}}
+    token = JWT.encode data, private_key, 'HS512'
+
+    assert_requested :post, endpoint,
+      body: {
+        'key': '123',
+        'token': token,
+      },
+      times: 1
+  end
+
+end
