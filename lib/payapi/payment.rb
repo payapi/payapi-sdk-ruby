d3b9fc6997bf23d167e889b09ed9962841360c89
Marko <marko@payapi.io>
2016-03-09 13:58:55 +0200

    Updating client

--- lib/payapi/payment.rb
+++ lib/payapi/payment.rb
@@ -5,73 +5,73 @@ module PayApi
   class Payment
     attr_reader :params, :data
     #params = {
-    #  "payment": {
-    #    "cardHolderEmail": "cardholder@example.com",
-    #    "cardHolderName": "John Smith",
-    #    "paymentMethod": "mastercard",
-    #    "creditCardNumber": "4242 4242 4242 4242",
-    #    "ccv": "1234",
-    #    "expiresMonth": "2",
-    #    "expiresYear": "3016",
-    #    "locale": "en-US",
-    #    "ip": "::ffff:127.0.0.1"
+    #  payment: {
+    #    cardHolderEmail: "cardholder@example.com",
+    #    cardHolderName: "John Smith",
+    #    paymentMethod: "mastercard",
+    #    creditCardNumber: "4242 4242 4242 4242",
+    #    ccv: "1234",
+    #    expiresMonth: "2",
+    #    expiresYear: "3016",
+    #    locale: "en-US",
+    #    ip: "::ffff:127.0.0.1"
     #  },
-    #  "consumer": {
-    #    "name": "John Smith",
-    #    "co": "",
-    #    "streetAddress": "Delivery street 123",
-    #    "streetAddress2": "",
-    #    "postalCode": "90210",
-    #    "city": "New York",
-    #    "stateOrProvince": "",
-    #    "country": "USA"
+    #  consumer: {
+    #    name: "John Smith",
+    #    co: "",
+    #    streetAddress: "Delivery street 123",
+    #    streetAddress2: "",
+    #    postalCode: "90210",
+    #    city: "New York",
+    #    stateOrProvince: "",
+    #    country: "USA"
     #  },
-    #  "order": {
-    #    "sumInCentsIncVat": 322,
-    #    "sumInCentsExcVat": 300,
-    #    "vatInCents": 22,
-    #    "currency": "EUR",
-    #    "referenceId": "ref123",
-    #    "sumIncludingVat": "€3.22",
-    #    "sumExcludingVat": "€3.00",
-    #    "vat": "€0.22"
+    #  order: {
+    #    sumInCentsIncVat: 322,
+    #    sumInCentsExcVat: 300,
+    #    vatInCents: 22,
+    #    currency: "EUR",
+    #    referenceId: "ref123",
+    #    sumIncludingVat: "€3.22",
+    #    sumExcludingVat: "€3.00",
+    #    vat: "€0.22"
     #  },
-    #  "products": [
+    #  products: [
     #    {
-    #      "id": "bbc123456",
-    #      "quantity": 1,
-    #      "title": "Black bling cap",
-    #      "description": "Flashy fine cap",
-    #      "imageUrl": "https://example.com/black_bling_cap.png",
-    #      "category": "Caps and hats",
-    #      "priceInCentsIncVat": 122,
-    #      "priceInCentsExcVat": 100,
-    #      "vatInCents": 22,
-    #      "vatPercentage": "22%",
-    #      "priceIncludingVat": "€1.22",
-    #      "priceExcludingVat": "€1.00",
-    #      "vat": "€0.22"
+    #      id: "bbc123456",
+    #      quantity: 1,
+    #      title: "Black bling cap",
+    #      description: "Flashy fine cap",
+    #      imageUrl: "https://example.com/black_bling_cap.png",
+    #      category: "Caps and hats",
+    #      priceInCentsIncVat: 122,
+    #      priceInCentsExcVat: 100,
+    #      vatInCents: 22,
+    #      vatPercentage: "22%",
+    #      priceIncludingVat: "€1.22",
+    #      priceExcludingVat: "€1.00",
+    #      vat: "€0.22"
     #    },
     #    {
-    #      "id": "pbc123456",
-    #      "quantity": 1,
-    #      "title": "Pink bling cap",
-    #      "description": "Flashy fine cap",
-    #      "imageUrl": "https://example.com/pink_bling_cap.png",
-    #      "category": "Caps and hats",
-    #      "priceInCentsIncVat": 222,
-    #      "priceInCentsExcVat": 200,
-    #      "vatInCents": 22,
-    #      "vatPercentage": "22%",
-    #      "priceIncludingVat": "€2.22",
-    #      "priceExcludingVat": "€2.00",
-    #      "vat": "€0.22"
+    #      id: "pbc123456",
+    #      quantity: 1,
+    #      title: "Pink bling cap",
+    #      description: "Flashy fine cap",
+    #      imageUrl: "https://example.com/pink_bling_cap.png",
+    #      category: "Caps and hats",
+    #      priceInCentsIncVat: 222,
+    #      priceInCentsExcVat: 200,
+    #      vatInCents: 22,
+    #      vatPercentage: "22%",
+    #      priceIncludingVat: "€2.22",
+    #      priceExcludingVat: "€2.00",
+    #      vat: "€0.22"
     #    }
     #  ],
-    #  "callbacks": {
-    #    "success": "https://merchantserver.xyz/payments/success",
-    #    "failed": "https://merchantserver.xyz/payments/failed",
-    #    "chargeback": "https://merchantserver.xyz/payments/chargeback"
+    #  callbacks: {
+    #    success: "https://merchantserver.xyz/payments/success",
+    #    failed: "https://merchantserver.xyz/payments/failed",
+    #    chargeback: "https://merchantserver.xyz/payments/chargeback"
     #  }
     #};
 
